3e7feae961891329149d591ab9f5b9e52936de74
Kevin Sheard <kevinsheardg@gmail.com>
2023-01-07 18:33:32 +0100

    removing unused states folder

--- src/lib/components/states/section1/State0.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-import { fade } from 'svelte/transition';
-import ShiftBtn from "$lib/components/ShiftBtn.svelte";
-</script>
-
-<section transition:fade>
-    press <ShiftBtn />Helllo
-</section>
--- src/lib/components/states/section1/State1.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-import { fly } from 'svelte/transition';
-import ShiftBtn from "$lib/components/ShiftBtn.svelte";
-</script>
-
-<section in:fly={{ y: 200, duration: 250 }}>
-    press <ShiftBtn /> again
-</section>
